562e8b74aab22204fa481cac042fd2cfb761f2eb
Surjeet Singh <singhsurjeet2005@gmail.com>
2023-05-21 21:56:39 +0530

    created login and profile component. Login component with login form

--- myProjects/sikhism/src/app/app-routing.module.ts
+++ myProjects/sikhism/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    component: LoginComponent,
+    path: ''
+  },
+  {
+    component: ProfileComponent,
+    path: 'profile'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myProjects/sikhism/src/app/app.module.ts
+++ myProjects/sikhism/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
