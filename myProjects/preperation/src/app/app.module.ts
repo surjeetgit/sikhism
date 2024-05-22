6a8d2876df4583feac7fd82e3c47fe5b576a7f23
Surjeet Singh <singhsurjeet2005@gmail.com>
2024-05-22 18:50:39 +0530

    template driven form exercise completed.

--- myProjects/preperation/src/app/app.module.ts
+++ myProjects/preperation/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
